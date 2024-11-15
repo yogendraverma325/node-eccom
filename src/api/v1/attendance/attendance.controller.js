8cb8eaf17e370fdc4256deec018b134ccd4f2e0c
yogendra verma <yogendra.verma@teamcomputers.com>
2024-11-15 11:54:48 +0530

    attendance day night shift

--- src/api/v1/attendance/attendance.controller.js
+++ src/api/v1/attendance/attendance.controller.js
@@ -48,7 +48,7 @@ class AttendanceController {
         }
       }
 
-      const currentDate = moment("2024-11-14 09:22:48");
+      const currentDate = moment();
 
       const existEmployee = await db.employeeMaster.findOne({
         where: {
