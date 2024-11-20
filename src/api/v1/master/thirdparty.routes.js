1a8213f1a4cd2b024d23f6b39505bf9e64732711
yogendra verma <yogendra.verma@teamcomputers.com>
2024-11-20 14:26:48 +0530

    code merged with harish

--- src/api/v1/master/thirdparty.routes.js
+++ src/api/v1/master/thirdparty.routes.js
@@ -1,8 +1,9 @@
 import Express from "express";
-import thirdPartyController from "./thirdParty.controller.js";
+import ThirdPartyController from "./thirdParty.controller.js";
 import authentication from "../../../middleware/authentication.js";
 import authorization from "../../../middleware/authorization.js";
 
-export default Express.Router()
-  .get("/employeeData", thirdPartyController.employeeData)
-
+export default Express.Router().get(
+  "/employeeData",
+  ThirdPartyController.employeeData
+);
