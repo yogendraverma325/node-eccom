596ddaf324dcd7339546f46dfd292a8c3f0d8e60
devdims <dev.dims@teamcomputers.com>
2024-12-30 17:04:31 +0530

    fix: update attendance cron timing

--- src/services/cronService.js
+++ src/services/cronService.js
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import cronController from "../api/v1/cron/cron.controller.js";
 import attendanceController from "../api/v1/attendance/attendance.controller.js";
 
-cron.schedule("30 1 * * *", async () => {
+cron.schedule("30 3 * * *", async () => {
   await attendanceController.attedanceCron();
 });
 
