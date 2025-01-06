aa812889369308c210111aee5c0fb6a8519dd272
devdims <dev.dims@teamcomputers.com>
2025-01-06 10:40:57 +0530

    fix: salary Structure name undefine check

--- src/api/v1/payments/payment.controller.js
+++ src/api/v1/payments/payment.controller.js
@@ -1,143 +1,3 @@
-// import db from "../../../config/db.config.js";
-// import respHelper from '../../../helper/respHelper.js'
-
-// class PaymentController {
-//     async payElements(req, res) {
-//         try {
-//             const user = req.query.user
-
-//             const payElementsData = await db.payElements.findAll({
-//                 where: {
-//                     EmployeeId: (user) ? user : req.userId
-//                 },
-//                 attributes: { exclude: ['createdAt', 'createdBy', 'updatedBy', 'updatedAt', 'isActive'] },
-//                 include: [{
-//                     model: db.salaryComponent,
-//                     attributes: { exclude: ['createdAt', 'createdBy', 'updatedBy', 'updatedAt', 'isActive'] },
-//                 }]
-//             })
-
-//             return respHelper(res, {
-//                 status: 200,
-//                 data: payElementsData
-//             })
-//         } catch (error) {
-//             console.log(error)
-//             return respHelper(res, {
-//                 status: 500
-//             })
-//         }
-//     }
-
-//     async paySlips(req, res) {
-//         try {
-//             const user = req.query.user
-//             const financialYear = req.query.financialYear
-
-//             const paySlip = await db.paySlips.findAll({
-//                 where:
-//                 {
-//                     EmployeeId: (user) ? user : req.userId,
-//                     paySlipFinancialYear: financialYear
-//                 },
-//                 order: [['createdAt', 'desc']],
-//                 attributes: { exclude: ['createdAt', 'createdBy'] },
-//                 include: [{
-//                     model: db.employeeMaster,
-//                     attributes: ['name', 'empCode', 'email', 'designation_id', 'departmentId', 'panNo', 'esicNo', 'uanNo', 'pfNo', 'employeeType'],
-//                     include: [{
-//                         model: db.departmentMaster,
-//                         required: true,
-//                         attributes: ["departmentCode", "departmentName"]
-//                     },
-//                     {
-//                         model: db.designationMaster,
-//                         required: false,
-//                         attributes: ['name']
-//                     },
-//                     {
-//                         model: db.jobDetails,
-//                         attributes: ['dateOfJoining']
-//                     }
-//                     ]
-//                 },
-//                 {
-//                     model: db.paySlipComponent,
-//                     attributes: { exclude: ['createdAt', 'createdBy', 'updatedBy', 'updatedAt', 'isActive'] },
-//                 }]
-//             })
-
-//             return respHelper(res, {
-//                 status: 200,
-//                 data: paySlip
-//             })
-
-//         } catch (error) {
-//             console.log(error)
-//             return respHelper(res, {
-//                 status: 500
-//             })
-//         }
-//     }
-
-//     async payPackage(req, res) {
-//         try {
-//             const user = req.query.user
-//             const financialYear = req.query.financialYear
-
-//             const payPackage = await db.payPackage.findAll({
-//                 where: {
-//                     EmployeeId: (user) ? user : req.userId,
-//                     payPackageFinancialYear: financialYear
-//                 },
-//                 order: [['createdAt', 'desc']],
-//                 attributes: { exclude: ['createdAt', 'createdBy', 'updatedBy', 'updatedAt', 'isActive'] },
-//             })
-
-//             return respHelper(res, {
-//                 status: 200,
-//                 data: payPackage
-//             })
-
-//         } catch (error) {
-//             console.log(error)
-//             return respHelper(res, {
-//                 status: 500
-//             })
-//         }
-//     }
-
-//     async ctcProration(req, res) {
-//         try {
-//             const payPackageAutoId = req.query.payPackageAutoId
-
-//             const payElements = await db.payElements.findAll({
-//                 where: {
-//                     payPackageAutoId
-//                 },
-//                 include: [{
-//                     model: db.salaryComponent,
-//                     attributes: { exclude: ['createdAt', 'createdBy', 'updatedBy', 'updatedAt', 'isActive'] },
-//                 }],
-//                 attributes: { exclude: ['createdAt', 'createdBy', 'updatedBy', 'updatedAt', 'isActive'] },
-//             })
-
-//             return respHelper(res, {
-//                 status: 200,
-//                 data: payElements
-//             })
-
-//         } catch (error) {
-//             console.log(error)
-//             return respHelper(res, {
-//                 status: 500
-//             })
-//         }
-//     }
-// }
-
-// export default new PaymentController();
-
 import { Op, where } from "sequelize";
 import db from "../../../config/db.config.js";
 import respHelper from "../../../helper/respHelper.js";
@@ -158,11 +18,7 @@ import path from "path"; // Import the path module
 import moment from "moment";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-//import moment, { now } from "moment";
-
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
 class PaymentController {
   async payElements(req, res) {
     try {
@@ -895,6 +751,16 @@ class PaymentController {
           ],
         });
 
+        console.log(structureDetails);
+
+        
+        if(structureDetails.length==0)
+        {
+          return respHelper(res, {
+            status: 400,
+            msg: "Salary Structure With Name "+employee["Salary Structure"]+" Not Found.",
+          });
+        }
         const error = await validator.createDynamicPayPackageSchema(
           structureDetails,
           employee
