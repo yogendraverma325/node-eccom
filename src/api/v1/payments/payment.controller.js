354a144aa2d3e169495c4d852f1f23a996a2f19f
devdims <dev.dims@teamcomputers.com>
2025-01-09 14:24:47 +0530

    fix: some extrapaymetn changes employee and paymonth dynamic

--- src/api/v1/payments/payment.controller.js
+++ src/api/v1/payments/payment.controller.js
@@ -3859,7 +3859,8 @@ async function processSalary(data) {
       //   raw: true,
       // });
 
-      let allDeductionQuery = `SELECT SUM(paymentAmount) AS totalExtraPayment, GROUP_CONCAT(category,'(',paymentAmount,')'  ORDER BY category SEPARATOR ' | ') AS paymentCategories FROM extrapayment WHERE paymentMonth = '2024-05' AND EmployeeId = 1982;`;
+      let allDeductionQuery = `SELECT SUM(paymentAmount) AS totalExtraPayment, GROUP_CONCAT(category,'(',paymentAmount,')'  ORDER BY category SEPARATOR ' | ') AS paymentCategories FROM extrapayment WHERE paymentMonth = '${result[0][0].payMonth}' AND EmployeeId = ${employee};`;
+      
       let extraPaymentAmount = await db.sequelize.query(allDeductionQuery);
       const ptAmount1 =
         ptDeducationDetails && ptDeducationDetails.lwfApplicable == 1
