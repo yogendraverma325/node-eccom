98bfc5dfcdc0b63a89d05746f3eebff168fd6633
Manish Maurya <manishmaurya@teamcomputers.com>
2025-01-14 11:39:20 +0530

    Solved Issue

--- src/api/v1/user/user.controller.js
+++ src/api/v1/user/user.controller.js
@@ -5062,7 +5062,7 @@ class UserController {
           id: user.dataValues.id,
           empCode: user.dataValues.empCode,
           name: user.dataValues.name,
-          rosterLimit: (user) ? user.dataValues.attendancePolicymaster.attendaceRosterLimitForPreviousDays : 0,
+          rosterLimit: (user.dataValues.attendancePolicymaster) ? user.dataValues.attendancePolicymaster.attendaceRosterLimitForPreviousDays : 0,
           attendanceRosterData: await rosterData()
         })
 
