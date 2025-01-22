80e6a96bf1cb99d7c3cf3cd2bbd85fac059c6b49
yogendra verma <yogendra.verma@teamcomputers.com>
2025-01-22 04:42:49 +0000

    cron now running for all

--- src/api/v1/attendance/attendance.controller.js
+++ src/api/v1/attendance/attendance.controller.js
@@ -2991,7 +2991,7 @@ class AttendanceController {
         ],
         where: {
           isActive: 1,
-          id: [1144, 3900],
+          //id: [1144, 3900],
         },
       });
       let nightwala = 0;
