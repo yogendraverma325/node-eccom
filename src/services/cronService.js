95c7f57deacca65c1507c214f691e543cd76ead1
yogendra verma <yogendra.verma@teamcomputers.com>
2025-02-25 11:03:56 +0530

    all merged to payroll uat release

--- src/services/cronService.js
+++ src/services/cronService.js
@@ -2,14 +2,14 @@ import cron from "node-cron";
 import cronController from "../api/v1/cron/cron.controller.js";
 import attendanceController from "../api/v1/attendance/attendance.controller.js";
 import helper from "../helper/helper.js";
-cron.schedule("30 1 * * *", async () => {
+cron.schedule("30 5 * * *", async () => {
 	await attendanceController.attedanceCron();
 });
-cron.schedule("10 2 * * *", async () => {
+cron.schedule("10 3 * * *", async () => {
 	await cronController.leaveActivation();
 	await helper.leaveLapse();
 });
-cron.schedule("10 3 * * *", async () => {
+cron.schedule("10 5 * * *", async () => {
 	await helper.leaveCreditMonthCron();
 	await helper.leaveRefil();
 });
