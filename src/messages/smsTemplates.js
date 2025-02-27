048d93ff8ab0ef2714b80fe38a2708ed48d81d07
Manish Maurya <manishmaurya@teamcomputers.com>
2025-02-27 15:56:18 +0530

    Added SMS Configuration in TARA

---
+++ src/messages/smsTemplates.js
@@ -0,0 +1,12 @@
+
+const forgotPasswordSMS = (data) => {
+	return {
+		template: `Dear User, Your Verification Code for Samadhan Mukti is ${data.otp}. Please do not share with others. It remains valid for 5 minutes.`,
+		templateId: "1007098871521547880"
+	}
+}
+
+
+export default {
+	forgotPasswordSMS
+}
