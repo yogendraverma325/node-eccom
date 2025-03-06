cc3a3a49482624f191216454aeb3e9642d16df7c
yogendra verma <yogendra.verma@teamcomputers.com>
2025-03-06 14:30:37 +0530

    cron service time changed

--- src/services/cronService.js
+++ src/services/cronService.js
@@ -2,10 +2,10 @@ import cron from "node-cron";
 import cronController from "../api/v1/cron/cron.controller.js";
 import attendanceController from "../api/v1/attendance/attendance.controller.js";
 import helper from "../helper/helper.js";
-cron.schedule("30 1 * * *", async () => {
+cron.schedule("30 3 * * *", async () => {
 	await attendanceController.attedanceCron();
 });
-cron.schedule("30 3 * * *", async () => {
+cron.schedule("30 5 * * *", async () => {
 	await cronController.leaveActivation();
 	//await helper.leaveLapse();
 });
