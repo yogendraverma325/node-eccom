8fb911fe75af8e6dfe81894ed75c83a147de95df
Manish Maurya <manishmaurya@teamcomputers.com>
2025-03-12 10:36:39 +0530

    Fixed Issue

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -115,7 +115,7 @@ const mailService = async (data) => {
 			time: "",
 			html: data.html,
 			cc: data.cc ? data.cc.split(",") : [],
-			attachments: (data.attachments.length > 0) ? data.attachments : [],
+			attachments: (data.attachments && data.attachments.length > 0) ? data.attachments : [],
 		});
 
 		const response = await axios.post(
