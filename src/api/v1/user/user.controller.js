064c3608aa3b465c9b1855993b2437842957b37d
Jay Prakash Singh <19737@teamcomputers.com>
2025-03-20 16:24:52 +0530

    Show created by in company location and manager history on employment details page

--- src/api/v1/user/user.controller.js
+++ src/api/v1/user/user.controller.js
@@ -5357,6 +5357,11 @@ class UserController {
 									},
 								],
 							},
+							{
+								model: db.employeeMaster,
+								as: "officeLocationHistoryCreatedBy",
+								attributes: ["id", "name"],
+							},
 							// { model: db.companyLocationMaster, as: 'officeLocationChangesFrom', attributes: ['companyLocationCode', 'address1'],
 							//   include: [
 							//       { model: db.countryMaster, attributes: ['countryId', 'countryName', 'countryCode'] },
@@ -5411,6 +5416,7 @@ class UserController {
 									},
 								],
 							},
+							{ model: db.employeeMaster, as: 'managerHistoryCreatedBy', attributes: ['id', 'name', 'empCode'] },
 							// { model: db.employeeMaster, as: 'managerChangesFrom', attributes: ['id', 'name', 'empCode' ] },
 						],
 						where: { needAttendanceCron: 0, employeeId: userId },
