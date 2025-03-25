405a080c4ce559fb6e9932912250039d8d727017
Manish Maurya <manishmaurya@teamcomputers.com>
2025-03-25 12:03:05 +0530

    Resolved Mail Issue in Separation

--- src/api/v1/user/user.controller.js
+++ src/api/v1/user/user.controller.js
@@ -19,7 +19,7 @@ class UserController {
 				attributes: ["id", "empCode", "name", "firstName", "lastName", "email"],
 				where: {
 					isActive: 1,
-					...(companyId && { companyId:companyId }),
+					...(companyId && { companyId: companyId }),
 					[Op.or]: [
 						{ empCode: { [Op.like]: `%${search}%` } },
 						{ name: { [Op.like]: `%${search}%` } },
@@ -709,8 +709,8 @@ class UserController {
 						where: Object.assign(
 							!["ADMIN", "HR_OPS"].includes(req.userRole)
 								? {
-										manager: req.userId,
-									}
+									manager: req.userId,
+								}
 								: {},
 							{
 								isActive: 1,
@@ -1144,10 +1144,10 @@ class UserController {
 				finalStatus: 2,
 				empAttachment: result.attachment
 					? await helper.fileUpload(
-							result.attachment,
-							`separation_attachment_${d}`,
-							`uploads/${existUser.dataValues.empCode}`,
-						)
+						result.attachment,
+						`separation_attachment_${d}`,
+						`uploads/${existUser.dataValues.empCode}`,
+					)
 					: null,
 				empSubmissionDate: moment(),
 				createdDt: moment(),
@@ -1355,10 +1355,10 @@ class UserController {
 					l1Remark: result.l1Remark,
 					l1Attachment: result.attachment
 						? await helper.fileUpload(
-								result.attachment,
-								`separation_attachment_${d}`,
-								`uploads/${separationData.dataValues.employee.empCode}`,
-							)
+							result.attachment,
+							`separation_attachment_${d}`,
+							`uploads/${separationData.dataValues.employee.empCode}`,
+						)
 						: null,
 					l1SubmissionDate: moment(),
 					pendingAt: separationData.dataValues.employee.buHRId,
@@ -1471,7 +1471,7 @@ class UserController {
 					},
 					{
 						model: db.companyMaster,
-						attributes: ["companyName"],
+						attributes: ["companyName", "senderEmail", "companyLogo"],
 					},
 					{
 						model: db.employeeMaster,
@@ -1594,6 +1594,8 @@ class UserController {
 				JSON.stringify({
 					email: existUser.dataValues.email,
 					companyName: existUser.dataValues.companymaster.companyName,
+					senderEmail: existUser.dataValues.companymaster.senderEmail,
+					companyLogo: existUser.dataValues.companymaster.companyLogo
 				}),
 			);
 
@@ -1606,6 +1608,8 @@ class UserController {
 					empCode: existUser.dataValues.empCode,
 					bu: existUser.dataValues.bumaster.buName,
 					managerName: existUser.dataValues.managerData.name,
+					senderEmail: existUser.dataValues.companymaster.senderEmail,
+					companyLogo: existUser.dataValues.companymaster.companyLogo
 				}),
 			);
 
@@ -2353,7 +2357,7 @@ class UserController {
 							},
 							{
 								model: db.companyMaster,
-								attributes: ["companyName"],
+								attributes: ["companyName", "senderEmail", "companyLogo"],
 							},
 							{
 								model: db.designationMaster,
@@ -2473,10 +2477,10 @@ class UserController {
 					l2Remark: result.l2Remark,
 					l2Attachment: result.attachment
 						? await helper.fileUpload(
-								result.attachment,
-								`separation_attachment_${d}`,
-								`uploads/${separationData.dataValues.employee.empCode}`,
-							)
+							result.attachment,
+							`separation_attachment_${d}`,
+							`uploads/${separationData.dataValues.employee.empCode}`,
+						)
 						: null,
 					l2SubmissionDate: moment(),
 					l2RequestStatus: "Approved",
@@ -2705,6 +2709,8 @@ class UserController {
 					companyName:
 						separationData.dataValues.employee.companymaster.companyName,
 					lastWorkingDay: result.l2LastWorkingDay,
+					senderEmail: separationData.dataValues.employee.companymaster.senderEmail,
+					companyLogo: separationData.dataValues.employee.companymaster.companyLogo,
 				}),
 			);
 
@@ -2733,6 +2739,8 @@ class UserController {
 						personalMailID: separationData.dataValues.employee.personalEmail,
 						personalMobileNumber:
 							separationData.dataValues.employee.personalMobileNumber,
+						senderEmail: separationData.dataValues.employee.companymaster.senderEmail,
+						companyLogo: separationData.dataValues.employee.companymaster.companyLogo,
 					}),
 				);
 			}
@@ -2957,10 +2965,10 @@ class UserController {
 						regularizeStatus: { [Op.ne]: "Pending" },
 						...(fromDate &&
 							extendedToDate && {
-								createdAt: {
-									[db.Sequelize.Op.between]: [fromDate, extendedToDate],
-								},
-							}),
+							createdAt: {
+								[db.Sequelize.Op.between]: [fromDate, extendedToDate],
+							},
+						}),
 					},
 					include: [
 						{
@@ -2980,11 +2988,11 @@ class UserController {
 										...(search && { name: { [Op.like]: `%${search}%` } }),
 										...(type === "all"
 											? {
-													[Op.or]: [
-														//{ id: req.userId },
-														{ manager: req.userId },
-													],
-												}
+												[Op.or]: [
+													//{ id: req.userId },
+													{ manager: req.userId },
+												],
+											}
 											: { id: req.userId }),
 									},
 									include: [
@@ -3051,15 +3059,15 @@ class UserController {
 			const leaveApprovalCondition =
 				type === "self"
 					? {
-							//createdBy: req.userId,
-							isPending: 0,
-							isApproved: [1, 2, 0],
-							// isPending: 1,
-						}
+						//createdBy: req.userId,
+						isPending: 0,
+						isApproved: [1, 2, 0],
+						// isPending: 1,
+					}
 					: {
-							isPending: 0,
-							//  isApproved:[1,2]
-						};
+						isPending: 0,
+						//  isApproved:[1,2]
+					};
 
 			const { count, rows: leaveRequests } =
 				await db.EmployeeLeaveHeader.findAndCountAll({
@@ -3070,26 +3078,26 @@ class UserController {
 							: { source: { [Op.ne]: "system_generated" } }),
 						...(fromDate &&
 							toDate && {
-								appliedFor: {
-									[db.Sequelize.Op.between]: [fromDate, toDate],
-								},
-							}),
+							appliedFor: {
+								[db.Sequelize.Op.between]: [fromDate, toDate],
+							},
+						}),
 						...(type === "all" && isSystemGenerated == 0
 							? {
-									[Op.or]: [
-										{
-											//pendingAt: req.userId,
-											source: { [Op.ne]: "system_generated" },
-										},
-									],
-								}
+								[Op.or]: [
+									{
+										//pendingAt: req.userId,
+										source: { [Op.ne]: "system_generated" },
+									},
+								],
+							}
 							: type === "all" && isSystemGenerated == 1
 								? {
-										[Op.or]: [
-											{ employeeId: req.userId },
-											{ pendingAt: req.userId, source: "system_generated" },
-										],
-									}
+									[Op.or]: [
+										{ employeeId: req.userId },
+										{ pendingAt: req.userId, source: "system_generated" },
+									],
+								}
 								: { employeeId: req.userId }), // Default case for non-"all" types
 					},
 					include: [
@@ -3398,7 +3406,7 @@ class UserController {
 							},
 							{
 								model: db.companyMaster,
-								attributes: ["companyName"],
+								attributes: ["companyName", "senderEmail", "companyLogo"],
 							},
 							{
 								model: db.designationMaster,
@@ -3703,6 +3711,8 @@ class UserController {
 							personalMailID: separationData.dataValues.employee.personalEmail,
 							personalMobileNumber:
 								separationData.dataValues.employee.personalMobileNumber,
+							senderEmail: separationData.dataValues.employee.companymaster.senderEmail,
+							companyLogo: separationData.dataValues.employee.companymaster.companyLogo,
 						}),
 					);
 				}
@@ -4679,7 +4689,7 @@ class UserController {
 			attributes: ["userId", "jobLevelId", "dateOfProbationEnd"],
 			include: {
 				model: db.employeeMaster,
-				attributes: ["id", "name", "confimationPolicyAutoId","companyId"],
+				attributes: ["id", "name", "confimationPolicyAutoId", "companyId"],
 				require: true,
 				where: {
 					isActive: 1,
@@ -5559,20 +5569,20 @@ class UserController {
 							: { source: { [Op.ne]: "system_generated" } }),
 						...(type === "all" && isSystemGenerated == 0
 							? {
-									[Op.or]: [
-										{
-											//updatedBy: req.userId,
-											source: { [Op.ne]: "system_generated" },
-										},
-									],
-								}
+								[Op.or]: [
+									{
+										//updatedBy: req.userId,
+										source: { [Op.ne]: "system_generated" },
+									},
+								],
+							}
 							: type === "all" && isSystemGenerated == 1
 								? {
-										[Op.or]: [
-											{ employeeId: req.userId },
-											{ updatedBy: req.userId, source: "system_generated" },
-										],
-									}
+									[Op.or]: [
+										{ employeeId: req.userId },
+										{ updatedBy: req.userId, source: "system_generated" },
+									],
+								}
 								: { employeeId: req.userId }), // Default case for non-"all" types
 					},
 					include: [
@@ -6058,39 +6068,39 @@ class UserController {
 				where: Object.assign(
 					!["ADMIN", "HR_OPS"].includes(req.userRole)
 						? {
-								manager: req.userId,
-							}
+							manager: req.userId,
+						}
 						: {},
 					{
 						isActive: 1,
 					},
 					search || search != ""
 						? {
-								[Op.or]: [
-									{
-										empCode: {
-											[Op.like]: `%${search}%`,
-										},
+							[Op.or]: [
+								{
+									empCode: {
+										[Op.like]: `%${search}%`,
 									},
-									{
-										name: {
-											[Op.like]: `%${search}%`,
-										},
+								},
+								{
+									name: {
+										[Op.like]: `%${search}%`,
 									},
-									{
-										email: {
-											[Op.like]: `%${search}%`,
-										},
+								},
+								{
+									email: {
+										[Op.like]: `%${search}%`,
 									},
-								],
-							}
+								},
+							],
+						}
 						: {},
 					selectedUser.length > 0
 						? {
-								id: {
-									[Op.notIn]: selectedUser,
-								},
-							}
+							id: {
+								[Op.notIn]: selectedUser,
+							},
+						}
 						: {},
 				),
 				attributes: ["id", "empCode", "name", "profileImage"],
@@ -6260,7 +6270,7 @@ class UserController {
 					name: user.dataValues.name,
 					rosterLimit: user.dataValues.attendancePolicymaster
 						? user.dataValues.attendancePolicymaster
-								.attendaceRosterLimitForPreviousDays
+							.attendaceRosterLimitForPreviousDays
 						: 0,
 					attendanceRosterData: await rosterData(),
 				});
