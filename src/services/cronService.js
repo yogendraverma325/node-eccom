7e0fae7ea4fb3f4236d0afe738554015a6e38a2f
devdims <dev.dims@teamcomputers.com>
2025-04-05 15:00:27 +0530

    cron time update

--- src/services/cronService.js
+++ src/services/cronService.js
@@ -3,7 +3,7 @@ import cronController from "../api/v1/cron/cron.controller.js";
 import attendanceController from "../api/v1/attendance/attendance.controller.js";
 import helper from "../helper/helper.js";
 
-cron.schedule("30 01 * * *", async () => {
+cron.schedule("30 03 * * *", async () => {
 	try {
 		if (process.env.ENABLE_BIOMETRIC_ATTENDANCE * 1) {
 			await cronController.biometricAttendance();
