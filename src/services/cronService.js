9af4d601d90c4d3f4f26ea9f6d3181f978c95adc
root <root@tara-vm-prod.asia-south1-a.c.it-apps-group-1.internal>
2025-04-11 14:21:06 +0530

    payroll uat realse

--- src/services/cronService.js
+++ src/services/cronService.js
@@ -3,7 +3,7 @@ import cronController from "../api/v1/cron/cron.controller.js";
 import attendanceController from "../api/v1/attendance/attendance.controller.js";
 import helper from "../helper/helper.js";
 
-cron.schedule("0 4 13 * * *", async () => {  // 12:01 AM
+cron.schedule("0 35 14 * * *", async () => {  // 12:01 AM
     await cronController.getEmpForWishes();
 });
 
