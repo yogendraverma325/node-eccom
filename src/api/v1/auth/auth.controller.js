e96b57b46feb3174a86f269f98afb3307f5f5edc
Jay Prakash Singh <19737@teamcomputers.com>
2025-04-24 13:49:47 +0530

    After login successfully from scan qr code, it will invalid

--- src/api/v1/auth/auth.controller.js
+++ src/api/v1/auth/auth.controller.js
@@ -243,7 +243,7 @@ class AuthController {
 
 			let secret = process.env.QR_SESSION_SECRET;
 			let signedToken = signSessionId(sessionId, secret);
-			const expiresAt = new Date(Date.now() + 50 * 1000); // 60 seconds from now
+			const expiresAt = new Date(Date.now() + 50 * 1000); // 50 seconds from now
 			await db.qrSessionHistory.create({ sessionId: signedToken, employeeId: id, createdBy: id, expiresAt: expiresAt, userAgent: userAgent });
 			return respHelper(res, { status: 200, msg: "Session id generated successfully", data: { sessionId: signedToken, loggedIn: false } });
 		}
@@ -259,7 +259,6 @@ class AuthController {
 	async authenticateSessionStatus(req, res) {
 		try {
             const { sessionId } = req.params;
-			const employeeId = req.userData.id;
 			let secret = process.env.QR_SESSION_SECRET;
 			let verifySessionId = verifySignedSessionId(sessionId, secret);
 
@@ -302,7 +301,7 @@ class AuthController {
 			let secret = process.env.QR_SESSION_SECRET;
 			let verifySessionId = verifySignedSessionId(sessionId, secret);
 
-			let verifySession = await db.qrSessionHistory.findOne({ where: { sessionId: sessionId }, attributes: ['qrSessionId', 'sessionId', 'employeeId', 'loggedIn', 'expiresAt'], raw: true });
+			let verifySession = await db.qrSessionHistory.findOne({ where: { sessionId: sessionId, loggedIn: false }, attributes: ['qrSessionId', 'sessionId', 'employeeId', 'loggedIn', 'expiresAt'], raw: true });
 			
 			if(!verifySession || !verifySessionId) {
 				return respHelper(res, {
