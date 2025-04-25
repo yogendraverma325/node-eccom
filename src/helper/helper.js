dddd1fe51404135bea50dcea5a0269491ba61fc6
Harish Prajapati <18950@teamcomputers.com>
2025-04-25 15:24:40 +0530

    common function added

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -3774,6 +3774,24 @@ const fetchEmployeeRole = (role, employeeId, actionBy) => {
 	}
 }
 
+async function convertEmptyStringsToNull(obj) {
+    if (Array.isArray(obj)) {
+        return Promise.all(obj.map(async (item) => await convertEmptyStringsToNull(item)));
+    } else if (obj && typeof obj === 'object' && obj !== null) {
+        const entries = await Promise.all(
+            Object.entries(obj).map(async ([key, value]) => {
+                const resolvedValue = await Promise.resolve(value); // Resolves the promise
+                return [key, await convertEmptyStringsToNull(resolvedValue)];
+            })
+        );
+        return Object.fromEntries(entries);
+    } else if (obj === '') {
+        return null;
+    }
+    return obj;
+}
+
+
 export default {
 	generateJwtToken,
 	checkFolder,
@@ -3833,5 +3851,6 @@ export default {
 	revokeAppliedLeave,
 	activeCompOffMoreThanLeave,
 	// Export by jay
-	fetchEmployeeRole
+	fetchEmployeeRole,
+	convertEmptyStringsToNull
 };
