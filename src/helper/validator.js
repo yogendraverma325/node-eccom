78a8f5dd19cda834107d5518e89a6ee283a617b9
root <root@Himanshu-11966.teamcomputers.com>
2025-04-30 03:36:59 +0000

    fix: some changes related to the gratuity upload validation

--- src/helper/validator.js
+++ src/helper/validator.js
@@ -1612,7 +1612,7 @@ const generatePaySlipSchema = Joi.object({
 const gratuityValidateSchama = Joi.object({
 	gratuityAutoId: Joi.number().integer().positive().optional(), // Auto-incremented primary key, not required in most cases.
 	EmployeeId: Joi.number().integer().positive().required(), // Employee ID is required.
-	payMonth: Joi.string().max(255).required(),
+	//payMonth: Joi.string().max(255).required(),
 	gratuityYears: Joi.number()
 		.precision(2)
 		.positive()
