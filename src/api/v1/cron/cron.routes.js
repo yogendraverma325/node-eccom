f888cbe5cace36717a5127da6393858652bd0a2e
root <root@tara-vm-prod.asia-south1-a.c.it-apps-group-1.internal>
2025-05-02 14:16:59 +0530

    error log file generated

--- src/api/v1/cron/cron.routes.js
+++ src/api/v1/cron/cron.routes.js
@@ -16,4 +16,5 @@ export default Express.Router()
 	//CONFIRMATION
 
 	.get("/biometricAttendance", cronController.biometricAttendance)
-	.get("/logs/:appName", cronController.getPm2Logs);
+	.get("/logs/:appName", cronController.getPm2Logs)
+	.get("/error_logs/:appName", cronController.error_logs);
