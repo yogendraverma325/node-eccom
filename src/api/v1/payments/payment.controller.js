4418f0b751481bc26b878c3c7a9a387a6166e0fb
Jay Prakash Singh <19737@teamcomputers.com>
2025-05-08 14:12:15 +0530

    Update attendance setting

--- src/api/v1/payments/payment.controller.js
+++ src/api/v1/payments/payment.controller.js
@@ -3964,6 +3964,7 @@ class PaymentController {
 				"PT Overrides": 15,
 				"LWF Overrides": 16,
 				"Notice Period Recovery": 17,
+				"Update Attendance Setting Sample": 18
 			};
 			const getKeyByValue = async (value) => {
 				const result = Object.keys(sheetName).find(
@@ -4060,7 +4061,7 @@ class PaymentController {
 				return res.end(report);
 			} else if (
 				getColumns.length > 0 &&
-				[1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].includes(
+				[1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].includes(
 					Number(exportSheetAutoId),
 				)
 			) {
