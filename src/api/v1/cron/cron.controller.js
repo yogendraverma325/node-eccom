792963a8fc193600c95198221be3ba40629bb256
root <root@tara-vm-prod.asia-south1-a.c.it-apps-group-1.internal>
2025-05-15 17:38:40 +0530

    policy assignment

--- src/api/v1/cron/cron.controller.js
+++ src/api/v1/cron/cron.controller.js
@@ -659,7 +659,7 @@ class CronController {
 					[Op.lte]: moment().format("YYYY-MM-DD"), // Fetch records where slaEndDate is less than today
 				},
 				confirmationGenerated: 0,
-				userId:5047
+				// userId:5047
 			},
 			attributes: ["userId", "jobLevelId", "dateOfProbationEnd"],
 			include: {
