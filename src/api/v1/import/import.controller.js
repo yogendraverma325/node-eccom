c02c1ebef8591251582ccb9c7637dea99a148169
Jay Prakash Singh <19737@teamcomputers.com>
2025-05-21 08:49:32 +0530

    Change key name in functional area function

--- src/api/v1/import/import.controller.js
+++ src/api/v1/import/import.controller.js
@@ -2692,7 +2692,7 @@ const employmentManagerDetails = async (payload, today) => {
 };
 
 const employmentFunctionalAreaDetails = async (payload, today) => {
-	let fromDate = convertExcelDate(payload.obj.jobLevelFromDate);
+	let fromDate = convertExcelDate(payload.obj.functionalFromDate);
 	const recordsExistForDate = await db.DepartmentEmploymentHistory.findOne({
 		raw: true,
 		where: {
